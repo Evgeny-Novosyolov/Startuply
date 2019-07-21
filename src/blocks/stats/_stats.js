fd883df54875eeb2ee1f483a9cbdf609f70e773a
Evgeny Novosyolov <Evgeny.Novosyolov.job@yandex.ru>
2019-07-21 20:57:40 +0300

    stats.js

--- src/blocks/stats/_stats.js
+++ src/blocks/stats/_stats.js
@@ -4,41 +4,50 @@ window.addEventListener('DOMContentLoaded', function () {
 
   const statsSection = document.querySelector(".stats");
 
-  window.addEventListener("mouseover", function () {
-        
-    if (event.target.classList.contains("stats")){
-      statsItem.forEach((item) => {
+  statsSection.addEventListener("mouseover", showNum)
 
-        let numMax = +item.dataset.num;
-        let numStats = 0;
-        const addNum = setInterval(function () {
-          if (numMax == numStats) {
-            clearInterval(addNum);
-          }
-          numStats++;
-          item.innerHTML = numStats - 1;
-        }, 5)
-      })
-    
-    }   
+ 
+ 
+ 
+  
+
+
+function showNum() {
+    statsItem.forEach((item) => {
+    let numMax = +item.dataset.num;
+    let numStats = 0;
+    const addNum = setInterval(function () {
+      if (numMax == numStats) {
+        clearInterval(addNum);
+      }
+      numStats++;
+      item.innerHTML = numStats - 1;
+    }, 5)
   });
+  statsSection.removeEventListener("mouseover", showNum)
+}
 
 
-    const btnVideo = document.querySelector('.play-video');
-    const btnParent = document.querySelector('.play__wrapper');
-    const videoBox = document.querySelector('.video');
 
-    btnVideo.addEventListener('click', function(){
-      btnParent.classList.add('btn-closs');
+
+
+
+
+  const btnVideo = document.querySelector('.play-video');
+  const btnParent = document.querySelector('.play__wrapper');
+  const videoBox = document.querySelector('.video');
+
+  btnVideo.addEventListener('click', function () {
+    btnParent.classList.add('btn-closs');
+    //videoBox.style.display = "block";
+    videoBox.classList.add('video--open');
+
+
+    setTimeout(function () {
+      btnParent.style.display = "none";
       //videoBox.style.display = "block";
-      videoBox.classList.add('video--open');
-       
-       
-      setTimeout(function(){
-        btnParent.style.display = "none";
-        //videoBox.style.display = "block";
-         
-      }, 800)
 
-    })
+    }, 800)
+
+  })
 });
